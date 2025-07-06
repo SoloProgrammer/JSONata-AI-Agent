735f5f32b3bc5a551e1b263d341c0f7a927298c6
pshinde <pshinde@Cogitate.us>
2025-07-06 21:40:34 +0530

    Removed NextApiResponse & Request Imports & usesage from test-connection api route

--- app/api/test-connection/route.ts
+++ app/api/test-connection/route.ts
@@ -1,15 +1,13 @@
 import { azureAIAgentsService } from "@/services/azure-ai-agents-service.service";
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 async function testAzureAIConnection(): Promise<{ success: boolean; message: string }> {
   return azureAIAgentsService.testConnection();
 }
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  console.log("-------", req.method);
+export async function GET(req: NextRequest, res: NextResponse) {
   if (req.method !== "GET") {
-    return res.status(405).end("Method Not Allowed");
+    return NextResponse.json({ message: "Method not allowed" }, { status: 500 });
   }
   const response = await testAzureAIConnection();
   return NextResponse.json(response, { status: 200 });
